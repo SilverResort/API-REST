f82e708dd600c613ec538c7a0e6cf5124360d985
Fares Bouslama <direction@silver-resorts.com>
2023-10-12 14:11:45 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+//Body Parser
+app.use(express.json())
+
+require('dotenv').config()
+
+const PORT=process.env.PORT
+
+app.listen(PORT, (err)=>{
+    err? console.log(err)
+    :
+    console.log(`Server is running on PORT ${PORT}`)
+})
+
+
+//connect to db
+const connect = require('./config/connectDB')
+
+connect()
+
+//require contact Router
+app.use('/api/contacts', require('./routes/contactRoute'))
